78f598b8679886e3b7da33985898eb9abe4986aa
Håkan Edling <hakan@tidyui.com>
2014-10-31 22:13:20 +0100

    Rewrote author edit with knockout.js

---
+++ Core/Piranha.Manager/Manager/Models/Author/ListItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Piranha.Manager.Models.Author
+{
+	public class ListItem
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Created { get; set; }
+		public string Updated { get; set; }
+	}
+}
